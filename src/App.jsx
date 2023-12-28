8a86b2319f10b426bcdc4398e2c437e48be0515f
Nick-Kevin <nicblou23@gmail.com>
2023-12-28 15:55:44 +0300

    install dependences

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
           Click on the Vite and React logos to learn more
         </p>
       </RevealOnScroll>
-      <div className='h-40'>
+      <div className='h-96'>
       </div>   
     </>
   )
