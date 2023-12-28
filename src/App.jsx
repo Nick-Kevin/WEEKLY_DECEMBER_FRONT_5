afa4183040c87e17dc568268a3c7af2c3c8da301
Nick-Kevin <nicblou23@gmail.com>
2023-12-28 14:53:07 +0300

    update reveal on scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,7 @@ import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-const RevealOnScroll = ({ children }) => {
-  const [isVisible, setIsVisible] = useState(false)
-  const ref = useRef(null)
-
-  useEffect(() => {
-    const onWindScroll = () => {
-      const element = ref.current
-      if (element) {
-        const { top } = element.getBoundingClientRect()
-        const isVisible = top < window.innerHeight
-        setIsVisible(isVisible)
-      }
-    }
-
-    window.addEventListener('scroll', onWindScroll)
-    return () => {
-      window.removeEventListener('scroll', onWindScroll)
-    }
-  }, [])
-
-  const classes = `transition-opacity duration-1000
-    ${ isVisible ? 'opacity-100' : 'opacity-0' } 
-  `
-
-  return (
-    <div ref={ref} className={classes}>
-      {children}
-    </div>
-  )
-}
+import RevealOnScroll from './components/animations/RevealOnScroll'
 
 function App() {
   const [count, setCount] = useState(0)
