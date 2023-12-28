3892420e3a50dacd7c582c71cb82980d3bbf8c8e
Nick-Kevin <nicblou23@gmail.com>
2023-12-28 15:10:02 +0300

    update revealOnScroll.jsx

--- src/components/animations/RevealOnScroll.jsx
+++ src/components/animations/RevealOnScroll.jsx
@@ -20,8 +20,8 @@ function RevealOnScroll ({ children }) {
       }
     }, [])
   
-    const classes = `transition-opacity duration-1000
-      ${ isVisible ? 'opacity-100' : 'opacity-0' } 
+    const classes = `transition-opacity duration-1000 reveal
+      ${ isVisible ? 'opacity-100 active' : 'opacity-0' } 
     `
   
     return (
