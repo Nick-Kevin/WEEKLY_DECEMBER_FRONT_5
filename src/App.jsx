51ee23f8288d57c211bbf4cec78d4821dbcb8de4
Nick-Kevin <nicblou23@gmail.com>
2023-12-28 16:46:53 +0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import RevealOnScroll from './components/animations/RevealOnScroll'
 import SetElementOnScroll from './components/animations/SetElementOnScroll'
-import { Bounce } from 'react-reveal'
+import { Fade, Slide } from 'react-reveal'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,25 +19,31 @@ function App() {
         </a>
       </div>
       <SetElementOnScroll>
-        <Bounce left>
-          <h1>Vite + React</h1>
-        </Bounce>
+          <h1>
+            <Fade left>
+              Vite + React
+            </Fade>
+          </h1>
       </SetElementOnScroll>
-      <RevealOnScroll>        
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
+      <div className='h-[200vh]'>
+      </div> 
+      <SetElementOnScroll>        
+        <Slide bottom>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
           </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </RevealOnScroll>
-      <div className='h-96'>
-      </div>   
+        </Slide>
+      </SetElementOnScroll>  
+      <div className='h-[200vh]'>
+      </div>    
     </>
   )
 }
