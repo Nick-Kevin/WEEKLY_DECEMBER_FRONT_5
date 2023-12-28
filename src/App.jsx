e08f15765f5e929115d8dec2eb3982b8385671df
Nick-Kevin <nicblou23@gmail.com>
2023-12-28 14:49:02 +0300

    add reveal on scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+const RevealOnScroll = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const onWindScroll = () => {
+      const element = ref.current
+      if (element) {
+        const { top } = element.getBoundingClientRect()
+        const isVisible = top < window.innerHeight
+        setIsVisible(isVisible)
+      }
+    }
+
+    window.addEventListener('scroll', onWindScroll)
+    return () => {
+      window.removeEventListener('scroll', onWindScroll)
+    }
+  }, [])
+
+  const classes = `transition-opacity duration-1000
+    ${ isVisible ? 'opacity-100' : 'opacity-0' } 
+  `
+
+  return (
+    <div ref={ref} className={classes}>
+      {children}
+    </div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='mb-[200vh]'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +47,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <RevealOnScroll>
+        <h1>Vite + React</h1>
+      </RevealOnScroll>      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
