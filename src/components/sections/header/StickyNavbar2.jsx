f560426f5bafc5ae80619e7c62d37bd0cb8b2fbf
Nick-Kevin <nicblou23@gmail.com>
2023-12-29 15:13:06 +0300

    last update

--- src/components/sections/header/StickyNavbar2.jsx
+++ src/components/sections/header/StickyNavbar2.jsx
@@ -55,16 +55,14 @@ export default function StickyNavbar2() {
   return (
     <Navbar className="bg-transparent relative z-50 px-0 py-4 lg:py-5">
       <div className="flex items-center justify-between mx-4 sm:mx-11 lg:mx-4 text-blue-gray-900">
-        <div className="flex items-center lg:hidden gap-x-1">
+        <div className="flex items-center lg:items-centergap-x-1">
           <Button
             variant="gradient"
-            className="inline-block rounded-md text-sm border-slate-300 border-[1px] px-2 py-1 hover:border-white"
+            className="inline-block rounded-md mr-[4.2rem] lg:hidden text-sm border-slate-300 border-[1px] px-2 py-1 hover:border-white"
           >
             <span>Sign in</span>
           </Button>
-        </div>
-        <div className="lg:flex lg:items-center">
-          <a class="mr-lg-3 lg:mr-4 color-fg-inherit flex-order-2" href="https://github.com/" aria-label="Homepage" data-ga-click="(Logged out) Header, go to homepage, icon:logo-wordmark">
+          <a class="lg:mr-4 color-fg-inherit flex-order-2" href="https://github.com/" aria-label="Homepage" data-ga-click="(Logged out) Header, go to homepage, icon:logo-wordmark">
             <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github">
               <path fill="white" d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
             </svg>
