1170e5ac874705e3c28f837d8eb7a2942a70d3ae
Nick-Kevin <nicblou23@gmail.com>
2023-12-29 13:19:01 +0300

    last update

--- src/components/sections/header/Dropdown.jsx
+++ src/components/sections/header/Dropdown.jsx
@@ -7,13 +7,16 @@ function classNames(...classes) {
 
 export default function Dropdown(props) {
   return (
-    <Menu as="div" className="relative w-full pt-5 inline-block text-left">
+    <Menu as="div" className="relative w-full pt-5 lg:pt-0 lg:ml-2 inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex text-xl text-slate-900 font-semibold justify-between items-center w-full py-1 pl-8 pr-14 lg:w-28 text-black none-border no-padding-left gap-x-1.5 rounded-md bg-transparent">
+        <Menu.Button className="inline-flex lg:flex text-xl lg:text-base text-slate-900 lg:text-white font-semibold lg:font-medium justify-between items-center w-full py-1 pl-8 lg:pl-0 pr-14 lg:pr-0 lg:w-max none-border no-padding-left gap-x-1.5 rounded-md bg-transparent">
           {props.label}
-          <svg opacity="0.5" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon rotate-[-90deg] octicon-chevron-down HeaderMenu-icon ml-1">
-              <path d="M12.78 5.22a.749.749 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.06 0L3.22 6.28a.749.749 0 1 1 1.06-1.06L8 8.939l3.72-3.719a.749.749 0 0 1 1.06 0Z"></path>
+          <svg opacity="0.5" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon hidden lg:block octicon-chevron-down HeaderMenu-icon">
+              <path stroke='white' d="M12.78 5.22a.749.749 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.06 0L3.22 6.28a.749.749 0 1 1 1.06-1.06L8 8.939l3.72-3.719a.749.749 0 0 1 1.06 0Z"></path>
           </svg>
+          <svg opacity="0.5" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon rotate-[-90deg] block lg:hidden octicon-chevron-down HeaderMenu-icon ml-1">
+              <path d="M12.78 5.22a.749.749 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.06 0L3.22 6.28a.749.749 0 1 1 1.06-1.06L8 8.939l3.72-3.719a.749.749 0 0 1 1.06 0Z"></path>
+          </svg>          
         </Menu.Button>
       </div>
 
@@ -26,7 +29,7 @@ export default function Dropdown(props) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute lg:left-[-20px] right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
--- src/components/sections/header/StickyNavbar2.jsx
+++ src/components/sections/header/StickyNavbar2.jsx
@@ -20,7 +20,7 @@ export default function StickyNavbar2() {
   }, []);
  
   const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:flex-row lg:items-center lg:gap-9">
+    <ul className="mt-2 mb-4 lg:my-0 flex flex-col gap-2 lg:flex-row lg:w-auto">
       <Dropdown
         label="Product"
         item1="Actions"
@@ -43,9 +43,9 @@ export default function StickyNavbar2() {
         as="li"
         variant="small"
         color="blue-gray"
-        className="flex items-center gap-x-2 p-1 font-medium"
+        className="flex items-center gap-x-2 p-1 lg:ml-2 font-medium"
       >
-        <a href="#" className="flex f-medium text-slate-900 text-xl font-semibold ml-8 mt-5 items-center">
+        <a href="#" className="flex f-medium text-slate-900 lg:text-white text-xl lg:text-base font-semibold lg:font-medium ml-8 mt-5 lg:m-0 items-center">
           Pricing
         </a>
       </Typography>
@@ -54,21 +54,23 @@ export default function StickyNavbar2() {
  
   return (
     <Navbar className="bg-transparent px-0 py-4">
-      <div className="flex items-center justify-between text-blue-gray-900">
-        <div className="flex items-center gap-x-1">
+      <div className="flex items-center justify-between mx-4 sm:mx-11 lg:mx-4 text-blue-gray-900">
+        <div className="flex items-center lg:hidden gap-x-1">
           <Button
             variant="gradient"
-            className="inline-block rounded-md text-sm lg:text-sm border-white border-1 px-2 py-1 hover:border-white"
+            className="inline-block rounded-md text-sm border-white border-[1px] px-2 py-1 hover:border-white"
           >
             <span>Sign in</span>
           </Button>
         </div>
-        <a class="mr-lg-3 color-fg-inherit flex-order-2" href="https://github.com/" aria-label="Homepage" data-ga-click="(Logged out) Header, go to homepage, icon:logo-wordmark">
-          <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github">
-            <path fill="white" d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-          </svg>
-        </a>
-        <div className="hidden lg:block">{navList}</div>       
+        <div className="lg:flex lg:items-center">
+          <a class="mr-lg-3 lg:mr-3 color-fg-inherit flex-order-2" href="https://github.com/" aria-label="Homepage" data-ga-click="(Logged out) Header, go to homepage, icon:logo-wordmark">
+            <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github">
+              <path fill="white" d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+            </svg>
+          </a>
+          <div className="hidden lg:block">{navList}</div>
+        </div>    
           <IconButton
             variant="text"
             className="w-6 text-inherit !flex !border-none active:!border-none focus:border-none hover:!border-none ml-4 sm:ml-12 md:ml-16 hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
@@ -108,9 +110,9 @@ export default function StickyNavbar2() {
           </IconButton>
       </div>
       <MobileNav open={openNav}>
-        <div className="fixed z-50 container bg-white ml-5 rounded-tl-md rounded-bl-md mx-auto">
+        <div className="fixed z-50 sm:w-80 sm:right-0 top-16 container bg-white ml-5 rounded-tl-md rounded-bl-md mx-auto">
           {navList}
-          <div className="ml-8 mr-10 mt-20 mb-4">
+          <div className="ml-8 mr-10 mt-20 sm:mt-40 mb-4">
             <button className="w-full flex justify-between rounded-md p-2 bg-header-search">
                 <div className="flex">
                   <MagnifyingGlassIcon className="w-4 opacity-20" />
