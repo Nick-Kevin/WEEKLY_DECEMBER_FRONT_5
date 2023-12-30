530044d64621475fa695e35624aa8edf3fbf821a
Nick-Kevin <nicblou23@gmail.com>
2023-12-30 13:29:01 +0300

    last update

--- src/components/utils/Content.jsx
+++ src/components/utils/Content.jsx
@@ -1,4 +1,7 @@
 import branchProductivity from '../../assets/images/git-branch-productivity-c304b83d09c7.svg'
+import RevealOnScroll from '../animations/RevealOnScroll'
+import SetElementOnScroll from '../animations/SetElementOnScroll'
+import { Fade } from 'react-reveal'
 
 function Content () {
     const arrowRight = (size) => {
@@ -27,31 +30,37 @@ function Content () {
             </div>
             <div className='w-11/12 ml-2 md:ml-6'>
                 <div className='w-full sm:w-10/12 md:w-6/12 py-3 px-4'>
-                    <div className='md:pr-4 my-5 sm:my-7 md:my-8 '>
-                        <h2 className='text-xl lg:text-[1.6rem] text-mona mb-6 text-slate-500'>
-                            <span className='text-white'>GitHub Copilot </span>
-                            empowers developers to complete
-                            tasks 55% faster with contextualized AI
-                            coding assistance across workflows.
-                        </h2>
-                        <a href="/" className='text-white mona-medium text-base lg:text-xl hover:underline hover:text-white'>
-                            Explore GitHub Copilot
-                            {arrowRight("24")}
-                        </a>
-                    </div>
+                    <SetElementOnScroll>
+                        <Fade right>
+                            <div className='md:pr-4 my-5 sm:my-7 md:my-8 '>
+                                <h2 className='text-xl lg:text-[1.6rem] text-mona mb-6 text-slate-500'>
+                                    <span className='text-white'>GitHub Copilot </span>
+                                    empowers developers to complete
+                                    tasks 55% faster with contextualized AI
+                                    coding assistance across workflows.
+                                </h2>
+                                <a href="/" className='text-white mona-medium text-base lg:text-xl hover:underline hover:text-white'>
+                                    Explore GitHub Copilot
+                                    {arrowRight("24")}
+                                </a>
+                            </div>
+                        </Fade>
+                    </SetElementOnScroll>
                 </div>
                 <div className='sm:w-8/12 md:w-7/12 lg:w-6/12 xl:w-5/12 py-3 px-4 mt-20 md:mt-40 lg:mt-32'>
-                    <div>
-                        <span className='mona-semibold text-xs py-1 px-2 text-accent-primary border-primary rounded-xl'>
-                            Did you know ?
-                        </span>
-                    </div>
-                    <h3 className='mona-medium text-accent-primary text-3xl sm:text-4xl md:text-5xl lg:text-[4.2rem] leading-tight mt-4 lg:mt-6'>22% increase</h3>
-                    <p className='text-white mona-medium text-xl lg:text-2xl mt-2 md:mt-6 mb-5 md:mb-6'>in developer productivity after three years with GitHub</p>
-                    <a href="/" className='text-white mona-medium text-base md:text-xl hover:underline hover:text-white'>
-                        Read the report
-                        {arrowRight("16")}
-                    </a>
+                    <RevealOnScroll>
+                        <div>
+                            <span className='mona-semibold text-xs py-1 px-2 text-accent-primary border-primary rounded-xl'>
+                                Did you know ?
+                            </span>
+                        </div>
+                        <h3 className='mona-medium text-accent-primary text-3xl sm:text-4xl md:text-5xl lg:text-[4.2rem] leading-tight mt-4 lg:mt-6'>22% increase</h3>
+                        <p className='text-white mona-medium text-xl lg:text-2xl mt-2 md:mt-6 mb-5 md:mb-6'>in developer productivity after three years with GitHub</p>
+                        <a href="/" className='text-white mona-medium text-base md:text-xl hover:underline hover:text-white'>
+                            Read the report
+                            {arrowRight("16")}
+                        </a>
+                    </RevealOnScroll>
                 </div>
             </div>
         </div>
