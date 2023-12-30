b8647e3a7d1d10213a481b214d8a91f5448234f4
Nick-Kevin <nicblou23@gmail.com>
2023-12-30 23:22:30 +0300

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,11 @@ function App() {
         <MainContent />        
       </main>
       <MyGlobe />
+      <div className="grid text-slate-500 px-4 lg:px-14 lg:w-8/12 gap-y-4 text-sm">
+          <p className="">1 This 7X times factor is based on data from the industry’s longest running analysis of fix rates Veracode State of Software Security 2023, which cites the average time to fix 50% of flaws as 198 days vs. GitHub’s fix rates of 72% of flaws with in 28 days which is at a minimum of 7X faster when compared.</p>
+          <p> 2 The Total Economic Impact™ Of GitHub Enterprise Cloud and Advanced Security, a commissioned study conducted by Forrester Consulting, 2022. Results are for a composite organization based on interviewed customers</p>
+          <p> 3 There are now 100 million developers around the world using GitHub. Read the blog post.</p>
+      </div>
       <Footer />
     </>
   );
--- src/components/sections/main/Globe.jsx
+++ src/components/sections/main/Globe.jsx
@@ -1,5 +1,11 @@
 import Globe from "react-globe.gl"
 import globeImg from "../../../assets/images/new-globe.png"
+import cat from '../../../assets/images/footer-mona-d1c861cd8018.png'
+import head from "../../../assets/images/footer-copilot-54114bfd1d20.png"
+import diamond from "../../../assets/images/footer-diamond-ed642fc95144.png"
+import blur from "../../../assets/images/footer-blur-8bc8e1f23df6.png"
+import orba from "../../../assets/images/footer-orb-a0438104a7a2.png"
+import star from "../../../assets/images/footer-star-36e5b5724973.png"
 
 function MyGlobe () {
     const N = 20
@@ -11,15 +17,21 @@ function MyGlobe () {
         color: [['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)], ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]],
     }))
     return (
-        <div>
+        <div className="relative">
             <Globe
                 globeImageUrl={globeImg}
+                backgroundColor="#0d1117"
                 arcsData={arcsData}
                 arcColor={'color'}
                 arcDashLength={() => Math.random()}
                 arcDashGap={() => Math.random()}
                 arcDashAnimateTime={() => Math.random() * 4000 + 500}
             />
+            <img src={cat} className="absolute hidden lg:block z-100 top-96 right-80 w-48" alt="cat" />
+            <img src={head} className="absolute hidden w-40 lg:block top-40 left-56" alt="head" />
+            <img src={star} className="absolute hidden w-10 lg:block" style={{top: '380px', left: '200px'}} alt="star" />
+            <img src={diamond} className="absolute hidden lg:block top-28 right-80 w-12" alt="diamond" />
+            <img src={orba} className="absolute hidden lg:block top-40 right-40 w-20" alt="orba" />
         </div>
     )
 }
